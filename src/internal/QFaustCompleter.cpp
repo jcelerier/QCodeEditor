1d6038084590781d008154e9de4d264abd25b45e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-07-28 22:06:07 -0400

    Better faust support

--- src/internal/QFaustCompleter.cpp
+++ src/internal/QFaustCompleter.cpp
@@ -16,49 +16,21 @@ QFaustCompleter::QFaustCompleter(QObject *parent) : QCompleter(parent)
 
     if (!fl.open(QIODevice::ReadOnly))
     {
-        // Fallback to hardcoded list if resource fails
-        list << "process" << "declare" << "import" << "library" << "environment" << "component"
-             << "with" << "letrec" << "where" << "case" << "waveform" << "soundfile" << "route"
-             << "inputs" << "outputs" << "seq" << "par" << "sum" << "prod"
-             << "button" << "checkbox" << "vslider" << "hslider" << "nentry" 
-             << "vgroup" << "hgroup" << "tgroup" << "vbargraph" << "hbargraph"
-             << "mem" << "prefix" << "int" << "float" << "rdtable" << "rwtable"
-             << "select2" << "select3" << "ffunction" << "fconstant" << "fvariable"
-             << "attach" << "lowest" << "highest"
-             << "abs" << "acos" << "asin" << "atan" << "atan2" << "ceil" << "cos"
-             << "exp" << "floor" << "fmod" << "log" << "log10" << "max" << "min"
-             << "pow" << "remainder" << "rint" << "round" << "sin" << "sqrt" << "tan";
+        return;
     }
-    else
-    {
-        QLanguage language(&fl);
 
-        if (language.isLoaded())
+    QLanguage language(&fl);
+
+    if (language.isLoaded())
+    {
+        auto keys = language.keys();
+        qDebug() << keys;
+        for (auto &&key : keys)
         {
-            auto keys = language.keys();
-            for (auto &&key : keys)
-            {
-                auto names = language.names(key);
-                list.append(names);
-            }
+            auto names = language.names(key);
+            list.append(names);
         }
     }
-
-    // Add some common library functions and patterns
-    list << "os.osc" << "os.sawtooth" << "os.triangle" << "os.square" << "os.phasor"
-         << "no.noise" << "no.pink_noise" << "no.lfnoise" << "no.multirandom"
-         << "fi.lowpass" << "fi.highpass" << "fi.bandpass" << "fi.notch" << "fi.peak_eq"
-         << "fi.resonbp" << "fi.resonhp" << "fi.resonlp" << "fi.filterbank"
-         << "de.delay" << "de.fdelay" << "de.sdelay" << "de.fdelayltv" << "de.echo"
-         << "dm.zita_light" << "dm.freeverb_demo" << "dm.stereo_freeverb"
-         << "ef.gate_mono" << "ef.gate_stereo" << "ef.compressor_mono" << "ef.limiter"
-         << "ef.distortion1" << "ef.cubicnl" << "ef.gate_compressor"
-         << "en.adsr" << "en.asr" << "en.ar" << "en.ahdshr" << "en.smoothEnvelope"
-         << "ba.time" << "ba.samp2sec" << "ba.sec2samp" << "ba.db2linear" << "ba.linear2db"
-         << "ba.selector" << "ba.selectn" << "ba.if" << "ba.bypass1" << "ba.bypass2"
-         << "si.smoo" << "si.smooth" << "si.polysmooth" << "si.bus" << "si.block"
-         << "ma.PI" << "ma.E" << "ma.SR" << "ma.T" << "ma.BS" << "ma.EPSILON"
-         << "ro.cross" << "ro.interleave" << "ro.hadamard";
 
     setModel(new QStringListModel(list, this));
     setCompletionColumn(0);
