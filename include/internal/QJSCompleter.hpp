31cbf8452e5eeec0c52936562e22dfde345d356a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-10-06 23:47:47 -0400

    QJSCompleter: allow to register custom completions

--- include/internal/QJSCompleter.hpp
+++ include/internal/QJSCompleter.hpp
@@ -2,7 +2,11 @@
 
 // Qt
 #include <QCompleter> // Required for inheritance
+#include <QStringList>
 #include <verdigris>
+
+class QObject;
+class QStringListModel;
 
 /**
  * @brief Class, that describes completer with
@@ -18,4 +22,24 @@ class QJSCompleter : public QCompleter
      * @param parent Pointer to parent QObject.
      */
     explicit QJSCompleter(QObject *parent = nullptr);
+
+    /**
+     * @brief Register a QObject instance to make its properties and methods available for completion.
+     * @param name The name/prefix used to access this object in JavaScript (e.g., "Foo" for "Foo.method()")
+     * @param object The QObject instance whose meta-object will be introspected
+     */
+    void registerObject(const QString& name, QObject* object);
+
+    /**
+     * @brief Unregister a previously registered object.
+     * @param name The name of the object to unregister
+     */
+    void unregisterObject(const QString& name);
+
+  private:
+    void updateCompletions();
+
+    QStringList m_baseCompletions;
+    QStringList m_registeredCompletions;
+    QStringListModel* m_model = nullptr;
 };
--- src/internal/QJSCompleter.cpp
+++ src/internal/QJSCompleter.cpp
@@ -5,6 +5,9 @@
 // Qt
 #include <QFile>
 #include <QStringListModel>
+#include <QMetaObject>
+#include <QMetaMethod>
+#include <QMetaProperty>
 
 #include <wobjectimpl.h>
 W_OBJECT_IMPL(QJSCompleter)
@@ -12,8 +15,6 @@ W_OBJECT_IMPL(QJSCompleter)
 QJSCompleter::QJSCompleter(QObject *parent) : QCompleter(parent)
 {
     // Setting up JavaScript types
-    QStringList list;
-
     Q_INIT_RESOURCE(qcodeeditor_resources);
     QFile fl(":/languages/js.xml");
 
@@ -33,12 +34,73 @@ QJSCompleter::QJSCompleter(QObject *parent) : QCompleter(parent)
     for (auto &&key : keys)
     {
         auto names = language.names(key);
-        list.append(names);
+        m_baseCompletions.append(names);
     }
 
-    setModel(new QStringListModel(list, this));
+    m_model = new QStringListModel(this);
+    setModel(m_model);
+    updateCompletions();
+
     setCompletionColumn(0);
     setModelSorting(QCompleter::CaseInsensitivelySortedModel);
     setCaseSensitivity(Qt::CaseSensitive);
     setWrapAround(true);
+}
+
+void QJSCompleter::registerObject(const QString& name, QObject* object)
+{
+    if (!object)
+        return;
+
+    // Get the meta-object for introspection
+    const QMetaObject* metaObj = object->metaObject();
+
+    // Extract all properties
+    m_registeredCompletions.reserve(metaObj->propertyCount() + metaObj->methodCount());
+    for (int i = 0; i < metaObj->propertyCount(); ++i)
+    {
+        m_registeredCompletions.append(QStringLiteral("%1.%2").arg(name, metaObj->property(i).name()));
+    }
+
+    // Extract all methods (slots, signals, and invokable methods)
+    for (int i = 0; i < metaObj->methodCount(); ++i)
+    {
+        QMetaMethod method = metaObj->method(i);
+        auto methodName = method.name();
+
+        // Skip internal Qt methods
+        if (methodName.startsWith("_q_"))
+            continue;
+        if (methodName.startsWith("destroyed") || methodName.startsWith("deleteLater"))
+            continue;
+
+        m_registeredCompletions.append(QStringLiteral("%1.%2").arg(name, methodName));
+    }
+
+    updateCompletions();
+}
+
+void QJSCompleter::unregisterObject(const QString& name)
+{
+    // Remove all completions that start with "name."
+    QString prefix = name + ".";
+    m_registeredCompletions.erase(
+        std::remove_if(m_registeredCompletions.begin(), m_registeredCompletions.end(),
+            [&prefix](const QString& completion) {
+                return completion.startsWith(prefix);
+            }),
+        m_registeredCompletions.end()
+    );
+
+    updateCompletions();
+}
+
+void QJSCompleter::updateCompletions()
+{
+    QStringList allCompletions = m_baseCompletions;
+    allCompletions.append(m_registeredCompletions);
+    allCompletions.removeDuplicates();
+
+    if (m_model)
+        m_model->setStringList(allCompletions);
 }
